314b568c30e0363c88f47d70880b44bc53e19dd7
SimonRohou <simon.rohou@gmail.com>
2020-06-03 17:10:29 +0200

    [macos] updated travis

--- scripts/dependencies/install_ibex.sh
+++ scripts/dependencies/install_ibex.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+set -x
+
 cd $HOME
 echo 'Installing IBEX in ' $HOME '...';
 if [ ! -e "ibex-lib/README.md" ]; then
   git clone -b develop https://github.com/ibex-team/ibex-lib.git ;
   cd ibex-lib ;
-  git checkout 47fc75a5e14eb2f353ae7e0b37ac0f9dad7801c0 ;
+  #git checkout 47fc75a5e14eb2f353ae7e0b37ac0f9dad7801c0 ;
   mkdir build && cd build ;
-  cmake -DCMAKE_CXX_FLAGS="-fPIC" -DCMAKE_C_FLAGS="-fPIC" -DCMAKE_INSTALL_PREFIX=$HOME/ibex-lib/build_install -DCMAKE_BUILD_TYPE=Debug .. ;
+  cmake -DCMAKE_CXX_FLAGS="-fPIC" -DCMAKE_C_FLAGS="-fPIC" -DCMAKE_INSTALL_PREFIX=$HOME/ibex-lib/build_install .. ;
   make ;
 else
   echo 'Using cached directory.' ;
