c0aeff3f9b6cff8f014039a7d29dbbbdb27bd820
SimonRohou <simon.rohou@gmail.com>
2020-06-06 11:54:45 +0200

    [travis] new test

--- scripts/dependencies/install_ibex.sh
+++ scripts/dependencies/install_ibex.sh
@@ -9,7 +9,7 @@ if [ ! -e "ibex-lib/README.md" ]; then
   cd ibex-lib ;
   #git checkout 47fc75a5e14eb2f353ae7e0b37ac0f9dad7801c0 ;
   mkdir build && cd build ;
-  cmake -DCMAKE_CXX_FLAGS="-fPIC" -DCMAKE_C_FLAGS="-fPIC" -DCMAKE_INSTALL_PREFIX=$HOME/ibex-lib/build_install .. ;
+  cmake -DCMAKE_CXX_FLAGS="-fPIC" -DCMAKE_C_FLAGS="-fPIC" -DINTERVAL_LIB=filib -DCMAKE_INSTALL_PREFIX=$HOME/ibex-lib/build_install .. ;
   make ;
 else
   echo 'Using cached directory.' ;
